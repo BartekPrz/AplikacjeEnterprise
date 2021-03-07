9b361ae3a37febd9b39e4bf5f1be6678126b6381
Bartosz Przybyl <bartolomeo369@gmail.com>
2021-03-07 17:52:24 +0100

    Add solution for lab1 task

---
+++ Lab1/src/main/java/ai/beans/ColorBean.java
@@ -0,0 +1,34 @@
+package ai.beans;
+
+public class ColorBean {
+    private String foregroundColor;
+    private String backgroundColor;
+    private String border;
+
+    public ColorBean() {
+    }
+
+    public String getForegroundColor() {
+        return foregroundColor;
+    }
+
+    public void setForegroundColor(String foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public String getBorder() {
+        return border;
+    }
+
+    public void setBorder(String border) {
+        this.border = border;
+    }
+}
