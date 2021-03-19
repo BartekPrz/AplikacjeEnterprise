2fe90da5c287e34abd60e778c02c7ccc4e1bed6c
Bartosz Przybyl <bartolomeo369@gmail.com>
2021-03-19 10:46:12 +0100

    Add solution for lab2 task

---
+++ Lab2/JSFLab/src/main/java/view/backing/LoginBean.java
@@ -0,0 +1,105 @@
+
+package view.backing;
+
+import java.util.ResourceBundle;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.html.HtmlCommandButton;
+import javax.faces.component.html.HtmlSelectBooleanCheckbox;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+
+@Named(value = "loginBean")
+@RequestScoped
+public class LoginBean {
+
+    private String username;
+    private String password;
+    private HtmlSelectBooleanCheckbox acceptCheckbox;
+    private HtmlCommandButton loginButton;
+    
+    public LoginBean() {
+    }
+
+    public String login() {
+        if (username.equals(password)) {
+            return "success";
+        } else if ("scott".equals(username) && "tiger".equals(password)) {
+            ResourceBundle resourceBundle = ResourceBundle.getBundle("ApplicationMessages");
+            String message = resourceBundle.getString("validation.oracle");
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+            return null;
+        } else {
+            return "failure";
+        }
+    }
+    
+    public void activateButton(ValueChangeEvent e) {
+        if (acceptCheckbox.isSelected())
+            loginButton.setDisabled(false);
+        else
+            loginButton.setDisabled(true);
+        
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.renderResponse();
+    }
+    
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the acceptCheckbox
+     */
+    public HtmlSelectBooleanCheckbox getAcceptCheckbox() {
+        return acceptCheckbox;
+    }
+
+    /**
+     * @param acceptCheckbox the acceptCheckbox to set
+     */
+    public void setAcceptCheckbox(HtmlSelectBooleanCheckbox acceptCheckbox) {
+        this.acceptCheckbox = acceptCheckbox;
+    }
+
+    /**
+     * @return the loginButton
+     */
+    public HtmlCommandButton getLoginButton() {
+        return loginButton;
+    }
+
+    /**
+     * @param loginButton the loginButton to set
+     */
+    public void setLoginButton(HtmlCommandButton loginButton) {
+        this.loginButton = loginButton;
+    }
+    
+}
